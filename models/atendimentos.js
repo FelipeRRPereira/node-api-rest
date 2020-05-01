6bda274f0bae6de34332b0b0e78e1812335974d5
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-05-01 10:58:57 -0300

    Adicionando validações de campos e retorno de erros.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,6 +5,30 @@ class Atendimentos {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const dataValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual.'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres.'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existeErros = erros.length;
+
+        if (existeErros) {
+            return res.status(400).json(erros);
+        }
+
         const atendimentoDatado = { ...atendimento, dataCriacao, data };
 
         const sql = 'INSERT INTO atendimentos SET ?';
