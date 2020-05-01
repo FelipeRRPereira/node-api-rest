3e6b129963f93bd1265b1bf6f88602ee49e08637
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2020-05-01 11:02:59 -0300

    Refactor e correções.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,46 +1,48 @@
-const conexao = require('../infraestrutura/conexao');
-const moment = require('moment');
+const conexao = require("../infraestrutura/conexao");
+const moment = require("moment");
 
 class Atendimentos {
-    adiciona(atendimento, res) {
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+  adiciona(atendimento, res) {
+    const dataCriacao = moment().format("YYYY-MM-DD HH:mm:ss");
+    const data = moment(atendimento.data, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:mm:ss"
+    );
 
-        const dataValida = moment(data).isSameOrAfter(dataCriacao);
-        const clienteValido = atendimento.cliente.length >= 5;
+    const dataValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteValido = atendimento.cliente.length >= 5;
 
-        const validacoes = [
-            {
-                nome: 'data',
-                valido: dataValida,
-                mensagem: 'Data deve ser maior ou igual a data atual.'
-            },
-            {
-                nome: 'cliente',
-                valido: clienteValido,
-                mensagem: 'Cliente deve ter pelo menos cinco caracteres.'
-            }
-        ]
+    const validacoes = [
+      {
+        nome: "data",
+        valido: dataValida,
+        mensagem: "Data deve ser maior ou igual a data atual.",
+      },
+      {
+        nome: "cliente",
+        valido: clienteValido,
+        mensagem: "Cliente deve ter pelo menos cinco caracteres.",
+      },
+    ];
 
-        const erros = validacoes.filter(campo => !campo.valido);
-        const existeErros = erros.length;
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existeErros = erros.length;
 
-        if (existeErros) {
-            return res.status(400).json(erros);
-        }
-
-        const atendimentoDatado = { ...atendimento, dataCriacao, data };
-
-        const sql = 'INSERT INTO atendimentos SET ?';
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro);           
-            } else {
-                res.status(201).json(resultados);              
-            }
-        })
+    if (existeErros) {
+      return res.status(400).json(erros);
     }
+
+    const atendimentoDatado = { ...atendimento, dataCriacao, data };
+
+    const sql = "INSERT INTO atendimentos SET ?";
+
+    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(201).json(resultados);
+      }
+    });
+  }
 }
 
-module.exports = new Atendimentos;
+module.exports = new Atendimentos();
